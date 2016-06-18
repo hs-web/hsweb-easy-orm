4adde0a6788772e798e643f33f289b97cd554995
周浩 <zh.sqy@qq.com>
2016-06-18 19:21:46 +0800

    mysql删除语句生成

--- src/main/java/org/hsweb/ezorm/render/dialect/MysqlDatabaseMeta.java
+++ src/main/java/org/hsweb/ezorm/render/dialect/MysqlDatabaseMeta.java
@@ -2,6 +2,7 @@ package org.hsweb.ezorm.render.dialect;
 
 import org.hsweb.ezorm.render.Dialect;
 import org.hsweb.ezorm.render.SqlRender;
+import org.hsweb.ezorm.render.support.mysql.MysqlDeleteSqlRender;
 import org.hsweb.ezorm.render.support.mysql.MysqlMetaCreateRender;
 
 public class MysqlDatabaseMeta extends AbstractDatabaseMeta {
@@ -13,6 +14,7 @@ public class MysqlDatabaseMeta extends AbstractDatabaseMeta {
     public void init() {
         super.init();
         renderMap.put(SqlRender.TYPE.META_CREATE, new MysqlMetaCreateRender());
+        renderMap.put(SqlRender.TYPE.DELETE, new MysqlDeleteSqlRender(Dialect.MYSQL));
     }
 
     public MysqlDatabaseMeta() {
