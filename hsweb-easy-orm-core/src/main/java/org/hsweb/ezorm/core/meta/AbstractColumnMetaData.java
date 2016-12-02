b2caac2ed25ae27f23c98838c3a6b80ce3b6a25c
zhouhao <zh.sqy@qq.com>
2016-12-02 18:22:30 +0800

    comment默认为""

--- hsweb-easy-orm-core/src/main/java/org/hsweb/ezorm/core/meta/AbstractColumnMetaData.java
+++ hsweb-easy-orm-core/src/main/java/org/hsweb/ezorm/core/meta/AbstractColumnMetaData.java
@@ -33,6 +33,7 @@ public abstract class AbstractColumnMetaData implements ColumnMetaData {
 
     @Override
     public String getComment() {
+        if (comment == null) comment = "";
         return comment;
     }
 
