6b73e6c44af9808eee256096800777f43bf1c620
zhou-hao <admin@hsweb.me>
2019-10-30 21:29:59 +0800

    增加onExecute方法，以支持自定义execute结果监听

--- hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/mapping/ReactiveUpdate.java
+++ hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/mapping/ReactiveUpdate.java
@@ -2,7 +2,11 @@ package org.hswebframework.ezorm.rdb.mapping;
 
 import reactor.core.publisher.Mono;
 
+import java.util.function.Function;
+
 public interface ReactiveUpdate<E> extends DSLUpdate<E, ReactiveUpdate<E>> {
 
     Mono<Integer> execute();
+
+    ReactiveUpdate<E> onExecute(Function<Mono<Integer>, Mono<Integer>>  consumer);
 }
--- hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/mapping/defaults/DefaultReactiveUpdate.java
+++ hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/mapping/defaults/DefaultReactiveUpdate.java
@@ -5,6 +5,10 @@ import org.hswebframework.ezorm.rdb.mapping.ReactiveUpdate;
 import org.hswebframework.ezorm.rdb.metadata.RDBTableMetadata;
 import org.hswebframework.ezorm.rdb.operator.dml.update.UpdateOperator;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class DefaultReactiveUpdate<E> extends DefaultUpdate<E, ReactiveUpdate<E>> implements ReactiveUpdate<E> {
 
@@ -12,8 +16,23 @@ public class DefaultReactiveUpdate<E> extends DefaultUpdate<E, ReactiveUpdate<E>
         super(table, operator, mapping);
     }
 
+    private Consumer<SignalType> consumer = (s) -> {
+    };
+
+    private Function<Mono<Integer>, Mono<Integer>> before = Function.identity();
+
     @Override
     public Mono<Integer> execute() {
-        return doExecute().reactive();
+        return before.apply(doExecute()
+                .reactive()
+                .doFinally(consumer));
     }
+
+    @Override
+    public ReactiveUpdate<E> onExecute(Function<Mono<Integer>, Mono<Integer>> consumer) {
+        this.before = this.before.andThen(consumer);
+        return this;
+    }
+
+
 }
