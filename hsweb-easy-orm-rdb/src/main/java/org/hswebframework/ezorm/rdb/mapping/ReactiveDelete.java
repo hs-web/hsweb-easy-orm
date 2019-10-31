9834e8c187f74e6b97f7043d16e32afe0e66b1da
zhou-hao <admin@hsweb.me>
2019-10-31 10:14:47 +0800

    delete 增加onExecute

--- hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/mapping/ReactiveDelete.java
+++ hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/mapping/ReactiveDelete.java
@@ -1,8 +1,11 @@
 package org.hswebframework.ezorm.rdb.mapping;
 
-import org.hswebframework.ezorm.core.Conditional;
 import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
 
 public interface ReactiveDelete extends DSLDelete<ReactiveDelete> {
     Mono<Integer> execute();
+
+    ReactiveDelete onExecute(Function<Mono<Integer>, Mono<Integer>> consumer);
 }
--- hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/mapping/defaults/DefaultReactiveDelete.java
+++ hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/mapping/defaults/DefaultReactiveDelete.java
@@ -7,13 +7,23 @@ import org.hswebframework.ezorm.rdb.metadata.RDBTableMetadata;
 import org.hswebframework.ezorm.rdb.operator.dml.delete.DeleteOperator;
 import reactor.core.publisher.Mono;
 
+import java.util.function.Function;
+
 public class DefaultReactiveDelete extends DefaultDelete<ReactiveDelete> implements ReactiveDelete {
     public DefaultReactiveDelete(RDBTableMetadata tableMetadata, DeleteOperator operator) {
-        super(tableMetadata,operator);
+        super(tableMetadata, operator);
     }
+
+    public Function<Mono<Integer>, Mono<Integer>> mapper = Function.identity();
 
     @Override
     public Mono<Integer> execute() {
-        return doExecute().reactive();
+        return mapper.apply(doExecute().reactive());
+    }
+
+    @Override
+    public ReactiveDelete onExecute(Function<Mono<Integer>, Mono<Integer>> mapper) {
+        this.mapper = mapper.andThen(mapper);
+        return this;
     }
 }
