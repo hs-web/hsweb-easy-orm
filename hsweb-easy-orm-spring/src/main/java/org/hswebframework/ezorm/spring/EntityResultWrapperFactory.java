a03a08abab87d01697dd19177e48831c2a3434fd
zhouhao <zh.sqy@qq.com>
2020-02-25 18:02:01 +0800

    删除spring

--- hsweb-easy-orm-spring/src/main/java/org/hswebframework/ezorm/spring/EntityResultWrapperFactory.java
+++
@@ -1,8 +0,0 @@
-package org.hswebframework.ezorm.spring;
-
-import org.hswebframework.ezorm.rdb.executor.wrapper.ResultWrapper;
-
-public interface EntityResultWrapperFactory {
-
-    <T> ResultWrapper<T, ?> getWrapper(Class<T> tClass);
-}
--- hsweb-easy-orm-spring/src/main/java/org/hswebframework/ezorm/spring/EntityTableMetadataResolver.java
+++
@@ -1,9 +0,0 @@
-package org.hswebframework.ezorm.spring;
-
-import org.hswebframework.ezorm.rdb.metadata.RDBTableMetadata;
-
-public interface EntityTableMetadataResolver {
-
-    RDBTableMetadata resolve(Class<?> entityClass);
-
-}
