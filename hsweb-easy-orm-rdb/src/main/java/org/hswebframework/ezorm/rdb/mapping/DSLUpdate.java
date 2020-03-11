00b4ecda828dadeb0f04249556482abf79983941
zhou-hao <admin@hsweb.me>
2020-03-11 14:47:36 +0800

    优化api

--- hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/mapping/DSLUpdate.java
+++ hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/mapping/DSLUpdate.java
@@ -21,7 +21,21 @@ public interface DSLUpdate<E, ME extends DSLUpdate> extends Conditional<ME> {
 
     ME setNull(String column);
 
-    public QueryParam toQueryParam();
+    default <R> ME set(MethodReferenceColumn<R> columnAndValue) {
+        return set(columnAndValue.getColumn(), columnAndValue.get());
+    }
+
+    default ME set(StaticMethodReferenceColumn<E> column, Object value) {
+        return set(column.getColumn(), value);
+    }
+
+    default ME setNull(StaticMethodReferenceColumn<E> column) {
+        return setNull(column.getColumn());
+    }
+
+    default ME setNull(MethodReferenceColumn<E> column) {
+        return setNull(column.getColumn());
+    }
 
     default ME includes(StaticMethodReferenceColumn<E>... columns) {
         return includes(Arrays
@@ -51,12 +65,6 @@ public interface DSLUpdate<E, ME extends DSLUpdate> extends Conditional<ME> {
                 .toArray(String[]::new));
     }
 
-    default <R> ME set(MethodReferenceColumn<R> columnAndValue) {
-        return set(columnAndValue.getColumn(), columnAndValue.get());
-    }
-
-    default ME set(StaticMethodReferenceColumn<E> column, Object value) {
-        return set(column.getColumn(), value);
-    }
+    public QueryParam toQueryParam();
 
 }
