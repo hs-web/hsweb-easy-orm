bcfe575fe1d1a33b4cb11941dfed5c80b5e99d5f
zhou-hao <admin@hsweb.me>
2020-09-27 09:45:55 +0800

    修复表名包含大写字母时无法获取字段问题

--- hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/supports/commons/RDBTableMetadataParser.java
+++ hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/supports/commons/RDBTableMetadataParser.java
@@ -284,7 +284,6 @@ public abstract class RDBTableMetadataParser implements TableMetadataParser {
 
     protected void applyColumnInfo(RDBColumnMetadata column, Record record) {
         record.getString("name")
-                .map(String::toLowerCase)
                 .ifPresent(name -> {
                     column.setName(name);
                     column.setProperty("old-name", name);
