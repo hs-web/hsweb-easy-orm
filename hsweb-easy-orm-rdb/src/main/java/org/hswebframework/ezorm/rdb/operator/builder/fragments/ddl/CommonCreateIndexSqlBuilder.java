cd0c3f3fc4483874ef072e3f8867d7c2d206732f
zhou-hao <admin@hsweb.me>
2021-07-01 12:10:23 +0800

    增加唯一索引支持

--- hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/operator/builder/fragments/ddl/CommonCreateIndexSqlBuilder.java
+++ hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/operator/builder/fragments/ddl/CommonCreateIndexSqlBuilder.java
@@ -1,6 +1,7 @@
 package org.hswebframework.ezorm.rdb.operator.builder.fragments.ddl;
 
 import org.hswebframework.ezorm.rdb.executor.SqlRequest;
+import org.hswebframework.ezorm.rdb.metadata.RDBColumnMetadata;
 import org.hswebframework.ezorm.rdb.metadata.RDBIndexMetadata;
 import org.hswebframework.ezorm.rdb.metadata.RDBTableMetadata;
 import org.hswebframework.ezorm.rdb.operator.builder.fragments.PrepareSqlFragments;
@@ -17,14 +18,18 @@ public class CommonCreateIndexSqlBuilder implements CreateIndexSqlBuilder {
         RDBIndexMetadata index = parameter.getIndex();
         RDBTableMetadata table = parameter.getTable();
         PrepareSqlFragments fragments = of()
-                .addSql("create index", index.getName(), "on", table.getFullName(), "(");
+                .addSql("create ", index.isUnique() ? "unique" : "", " index", index.getName(), "on", table.getFullName(), "(");
         int i = 0;
         for (RDBIndexMetadata.IndexColumn column : index.getColumns()) {
+            RDBColumnMetadata columnMetadata = table
+                    .getColumn(column.getColumn())
+                    .orElseThrow(() -> new UnsupportedOperationException("为定义的索引列:" + column.getColumn()));
+
             if (i++ != 0) {
                 fragments.addSql(",");
             }
-            fragments.addSql(table.getDialect().quote(column.getColumn()))
-                    .addSql(column.getSort().name());
+            fragments.addSql(table.getDialect().quote(columnMetadata.getName()))
+                     .addSql(column.getSort().name());
         }
         return fragments.addSql(")").toRequest();
     }
