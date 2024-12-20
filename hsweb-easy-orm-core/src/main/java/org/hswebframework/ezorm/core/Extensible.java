acf6453f76eca3e305bb628ed4b715b73bd0480f
zhouhao <zh.sqy@qq.com>
2024-12-20 10:29:34 +0800

    refactor: 优化

--- hsweb-easy-orm-core/src/main/java/org/hswebframework/ezorm/core/Extensible.java
+++ hsweb-easy-orm-core/src/main/java/org/hswebframework/ezorm/core/Extensible.java
@@ -40,4 +40,23 @@ public interface Extensible {
     @JsonAnySetter
     void setExtension(String property, Object value);
 
+    /**
+     * 方法引用方式设置扩展属性
+     * @param property 属性名
+     * @param value 属性值
+     * @param <T> 属性值类型
+     */
+    default <T> void setExtension(StaticMethodReferenceColumn<T> property, T value) {
+        setExtension(property.getColumn(), value);
+    }
+
+    /**
+     * 方法引用方式设置扩展属性
+     * @param property 属性名
+     * @param <T> 属性值类型
+     */
+    default <T> void setExtension(MethodReferenceColumn<T> property) {
+        setExtension(property.getColumn(), property.get());
+    }
+
 }
