71472e7a95d778706efb2e5a94eb9c204d3a4a27
zhouhao <zh.sqy@qq.com>
2024-12-20 09:37:36 +0800

    feat: 增加Extensible接口,可实现此接口来拓展字段.

--- hsweb-easy-orm-core/src/main/java/org/hswebframework/ezorm/core/Extensible.java
+++ hsweb-easy-orm-core/src/main/java/org/hswebframework/ezorm/core/Extensible.java
@@ -14,22 +14,28 @@ public interface Extensible {
 
     /**
      * 获取所有扩展属性
+     *
      * @return 扩展属性
      */
     @JsonAnySetter
-    Map<String, Object> getExtensions();
+    Map<String, Object> extensions();
 
     /**
      * 获取扩展属性
+     *
      * @param property 属性名
      * @return 属性值
      */
-    Object getExtension(String property);
+    default Object getExtension(String property) {
+        Map<String, Object> ext = extensions();
+        return ext == null ? null : ext.get(property);
+    }
 
     /**
      * 设置扩展属性
+     *
      * @param property 属性名
-     * @param value 属性值
+     * @param value    属性值
      */
     @JsonAnySetter
     void setExtension(String property, Object value);
