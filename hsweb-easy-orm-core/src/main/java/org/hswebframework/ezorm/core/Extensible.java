393cf82b733c7e379ed40a475d59ccf3536cb43f
zhouhao <zh.sqy@qq.com>
2024-12-27 19:48:58 +0800

    refactor: 优化对拓展实体支持

--- hsweb-easy-orm-core/src/main/java/org/hswebframework/ezorm/core/Extensible.java
+++ hsweb-easy-orm-core/src/main/java/org/hswebframework/ezorm/core/Extensible.java
@@ -41,11 +41,45 @@ public interface Extensible {
     @JsonAnySetter
     void setExtension(String property, Object value);
 
+    default void setExtension(String property, int value) {
+        setExtension(property, (Object) value);
+    }
+
+    default void setExtension(String property, long value) {
+        setExtension(property, (Object) value);
+    }
+
+    default void setExtension(String property, double value) {
+        setExtension(property, (Object) value);
+    }
+
+    default void setExtension(String property, float value) {
+        setExtension(property, (Object) value);
+    }
+
+    default void setExtension(String property, boolean value) {
+        setExtension(property, (Object) value);
+    }
+
+    default void setExtension(String property, byte value) {
+        setExtension(property, (Object) value);
+    }
+
+    default void setExtension(String property, char value) {
+        setExtension(property, (Object) value);
+    }
+
+    default void setExtension(String property, short value) {
+        setExtension(property, (Object) value);
+    }
+
+
     /**
      * 方法引用方式设置扩展属性
+     *
      * @param property 属性名
-     * @param value 属性值
-     * @param <T> 属性值类型
+     * @param value    属性值
+     * @param <T>      属性值类型
      */
     default <T> void setExtension(StaticMethodReferenceColumn<T> property, T value) {
         setExtension(property.getColumn(), value);
@@ -53,8 +87,9 @@ public interface Extensible {
 
     /**
      * 方法引用方式设置扩展属性
+     *
      * @param property 属性名
-     * @param <T> 属性值类型
+     * @param <T>      属性值类型
      */
     default <T> void setExtension(MethodReferenceColumn<T> property) {
         setExtension(property.getColumn(), property.get());
