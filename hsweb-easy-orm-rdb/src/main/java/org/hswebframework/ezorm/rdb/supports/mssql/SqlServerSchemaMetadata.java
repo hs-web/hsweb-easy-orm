16f459e4d9876347fb23adf8422c20fd6422dda0
zhouhao <zh.sqy@qq.com>
2025-11-22 16:11:30 +0800

    refactor: 优化函数支持

--- hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/supports/mssql/SqlServerSchemaMetadata.java
+++ hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/supports/mssql/SqlServerSchemaMetadata.java
@@ -4,6 +4,7 @@ import org.hswebframework.ezorm.rdb.codec.EnumValueCodec;
 import org.hswebframework.ezorm.rdb.metadata.RDBSchemaMetadata;
 import org.hswebframework.ezorm.rdb.metadata.RDBTableMetadata;
 import org.hswebframework.ezorm.rdb.metadata.dialect.Dialect;
+import org.hswebframework.ezorm.rdb.operator.builder.fragments.function.SimpleFunctionFragmentBuilder;
 
 public class SqlServerSchemaMetadata extends RDBSchemaMetadata {
 
@@ -16,14 +17,18 @@ public class SqlServerSchemaMetadata extends RDBSchemaMetadata {
         addFeature(new SqlServerIndexMetadataParser(this));
         addFeature(Dialect.MSSQL);
         addFeature(new SqlServerQuerySqlBuilder(this));
+        addFeature(new SimpleFunctionFragmentBuilder("stddev_pop", "stdevp", "标准差(总体)"));
+        addFeature(new SimpleFunctionFragmentBuilder("stddev_samp", "stdev", "标准差(样本)"));
+
+
     }
 
     @Override
     public RDBTableMetadata newTable(String name) {
-        RDBTableMetadata metadata=super.newTable(name);
+        RDBTableMetadata metadata = super.newTable(name);
         metadata.addFeature(new SqlServerBatchUpsertOperator(metadata));
-        metadata.setOnColumnAdded(column->{
-            if(column.getValueCodec() instanceof EnumValueCodec &&((EnumValueCodec) column.getValueCodec()).isToMask()){
+        metadata.setOnColumnAdded(column -> {
+            if (column.getValueCodec() instanceof EnumValueCodec && ((EnumValueCodec) column.getValueCodec()).isToMask()) {
                 column.addFeature(SqlServerEnumInFragmentBuilder.in);
                 column.addFeature(SqlServerEnumInFragmentBuilder.notIn);
             }
