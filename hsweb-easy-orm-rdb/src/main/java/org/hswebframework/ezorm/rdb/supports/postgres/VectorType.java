29827e0b363e4e70ad7b04e6505a8b18ae9d37d3
gyl <1554863190@qq.com>
2026-04-07 19:59:35 +0800

    fix: 修正可选依赖导致类不存在报错问题

--- hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/supports/postgres/VectorType.java
+++ hsweb-easy-orm-rdb/src/main/java/org/hswebframework/ezorm/rdb/supports/postgres/VectorType.java
@@ -61,14 +61,17 @@ public class VectorType implements DataType, ValueCodec<Object, Object>, DataTyp
         return toFloat(data);
     }
 
+    private static final String PG_OBJECT_CLASS = "org.postgresql.util.PGobject";
+    private static final String R2DBC_PG_OBJECT_CLASS = "io.r2dbc.postgresql.codec.Vector";
+
     private Float[] toFloat(Object data) {
         if (data == null) {
             return null;
         }
-        if (data instanceof Vector vector) {
+        if (R2DBC_PG_OBJECT_CLASS.equals(data.getClass().getName()) && data instanceof Vector vector) {
             return toFloatArray(vector.getVector());
         }
-        if (data instanceof PGobject vector) {
+        if (PG_OBJECT_CLASS.equals(data.getClass().getName()) && data instanceof PGobject vector) {
             return toFloatArray(vector.getValue());
         }
         return toFloatArray(data);
